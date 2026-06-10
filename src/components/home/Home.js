a8f1a8ea7f056ce1c02462c003ff3b384a080c44
Urvashi Kohale <55206809+urvashikohale@users.noreply.github.com>
2026-06-10 13:37:46 -0700

    Refactor Home component for improved structure

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,23 +8,23 @@ const Home = () => {
     <div className="container home" id="home">
       <div className="homeLeft">
         <div className="homeName">
-  <h2 className="hi">Hi there!</h2>
+          <div className="hi">Hi there 👋</div>
 
-  <h1 className="bigText">
-    I'm <span>Urvashi Kohale</span>
-  </h1>
+          <h1 className="bigText">
+            I'm <span>Urvashi Kohale</span>
+          </h1>
 
-  <div className="roleText">Software Engineer</div>
+          <div className="roleText">Software Engineer</div>
 
-  <p className="subText">
-    Software Engineer with industry experience building software applications,
-    cloud-based systems, and modern web platforms. Currently pursuing an MS in
-    Software Engineering at San Jose State University.
-  </p>
+          <p className="subText">
+            Software Engineer with industry experience building software applications,
+            cloud-based systems, and modern web platforms. Currently pursuing an MS in
+            Software Engineering at San Jose State University.
+          </p>
 
-  <div className="locationBadge">United States</div>
-</div>
-        
+          <div className="locationBadge">United States</div>
+        </div>
+
         <a href="#contact" className="homeButton">
           Contact Me
         </a>
@@ -33,7 +33,7 @@ const Home = () => {
       </div>
 
       <div className="homeRight">
-        <img src={IMG} style={{ width: "30rem" }} alt="profile" />
+        <img src={IMG} alt="profile" />
       </div>
     </div>
   );
