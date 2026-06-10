eca7abca8465428fa094eb23083385a2037bcdea
Urvashi Kohale <55206809+urvashikohale@users.noreply.github.com>
2026-06-10 13:24:22 -0700

    Modify Home component content and attributes
    Updated job title and added description in Home component.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,24 +5,42 @@ import IMG from "../../assets/transparent.png";
 
 const Home = () => {
   return (
-    <div className="container home  " id="home">
+    <div className="container home" id="home">
       <div className="homeLeft">
         <div className="homeName">
           <h2 className="hi">Hi there!</h2>
+
           <h1 className="bigText">
             I'm <span>Urvashi Kohale</span>
           </h1>
+
           <div className="wrapper">
-            <div className="staticText">FullStack Developer</div>
+            <div className="staticText">
+              Software Engineer
+            </div>
+          </div>
+
+          <div className="subText">
+            Software Engineer with industry experience developing software
+            applications, cloud-based systems, and modern web platforms.
+            Currently pursuing an MS in Software Engineering at San Jose State
+            University.
+          </div>
+
+          <div className="locationText">
+            United States
           </div>
         </div>
+
         <a href="#contact" className="homeButton">
           Contact Me
         </a>
+
         <Socialicons />
       </div>
+
       <div className="homeRight">
-        <img src={IMG} style={{ width: "30rem" }} />
+        <img src={IMG} style={{ width: "30rem" }} alt="profile" />
       </div>
     </div>
   );
